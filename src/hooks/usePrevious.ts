56c46faa6e55d88e3444661465b97d5081d1a6cd
puptup <putumoney@gmail.com>
2022-12-27 15:26:51 +0300

    refactor: change usePrevious types

--- src/hooks/usePrevious.ts
+++ src/hooks/usePrevious.ts
@@ -1,14 +1,14 @@
 import { ChildrenWithRef } from "@components/animated-toast-list/AnimatedToastList";
-import React, { useEffect, useRef } from "react";
+import { useEffect, useRef } from "react";
 
-const usePrevious = (value: ChildrenWithRef): ChildrenWithRef => {
-  const prevChildrenRef = useRef<React.ReactNode>();
+const usePrevious = (value: ChildrenWithRef): ChildrenWithRef | undefined => {
+  const prevChildrenRef = useRef<ChildrenWithRef>();
 
   useEffect(() => {
     prevChildrenRef.current = value;
   }, [value]);
 
-  return prevChildrenRef.current as ChildrenWithRef;
+  return prevChildrenRef.current;
 };
 
 export default usePrevious;
