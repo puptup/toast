671a75b52494aa4b92b4209161da20760fb5e4a9
puptup <putumoney@gmail.com>
2023-01-03 12:54:51 +0300

    fix: change rollup config

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,38 +1,40 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from 'rollup-plugin-typescript2';
-import babel from '@rollup/plugin-babel'
+import babel, {getBabelInputPlugin} from '@rollup/plugin-babel'
 import replace from '@rollup/plugin-replace';
 
-export default {
-  input: './src/index.tsx',
-  output: [
-    {
-      format: 'es'
-    }
-  ],
-  plugins: [
-    resolve({
-      extensions: ['.tsx', '.ts']
-    }),
-    commonjs(),
-    typescript({
-      typescript: require('ttypescript'),
-      tsconfigDefaults: {
-        compilerOptions: {
-          outDir: './build',
-          plugins: [
-            { "transform": "typescript-transform-paths" },
-            { "transform": "typescript-transform-paths", "afterDeclarations": true }
-          ],
-        }}
+import external from "rollup-plugin-peer-deps-external"
+import dts from "rollup-plugin-dts"
+
+
+export default [
+  {
+    input: "src/index.tsx",
+    output: [
+      {
+        file: "dist/index.js",
+        format: 'esm',
+      }
+    ],
+    plugins: [
+      resolve(),
+      external({includeDependencies: true}),
+      commonjs(),
+      replace({
+        preventAssignment: true,
+        'process.env.NODE_ENV': JSON.stringify('production')
       }),
-    babel({
-      exclude: './node_modules/**'
-    }),
-    replace({
-      preventAssignment: true,
-      'process.env.NODE_ENV': JSON.stringify('production')
-    }),
-  ]
-}
+      typescript({
+        typescript: require('ttypescript'),
+        tsconfigDefaults: {
+          compilerOptions: {
+            plugins: [
+              { "transform": "typescript-transform-paths" },
+              { "transform": "typescript-transform-paths", "afterDeclarations": true }
+            ],
+          }}
+      }),
+    ]
+  }
+]
